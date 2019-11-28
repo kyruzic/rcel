6e676a1542ddd4c8f5388a805bdb87148e21f030
Kyle Ruzic <kyruzic@gmail.com>
2019-11-27 22:20:01 -0700

    Now builds and renders a table with a newly created widget based off of the table widget that allows for borders to be drawn between columns

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,56 @@
 mod events;
 mod csv_utils;
+mod widgets;
 
-use std::error::Error;
 use std::io;
-use std::process;
-use std::iter;
-use std::collections::HashMap;
+use std::iter::Iterator;
+
 use termion::event::Key;
-use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use tui::backend::{Backend, TermionBackend};
-use tui::layout::{Constraint, Direction, Layout};
-use tui::style::{Color, Modifier, Style};
-use tui::widgets::{Block, Borders, Widget, Table, Row};
+use tui::layout::Constraint;
+use tui::style::{Color, Style};
+use tui::widgets::Widget;
 use tui::{Frame, Terminal};
 
 use events::{Event, Events};
 
-fn draw_rows<B>(f: &mut Frame<B>)
+use widgets::gridded_table::{GriddedTable, Row};
+
+
+fn draw_table<B>(f: &mut Frame<B>)
 where
     B: Backend,
 {
     // Getting data
-    let columns = csv_utils::read_csv_to_columns().unwrap();
     let csv_rows = csv_utils::read_csv_to_rows().unwrap();
     let headers = csv_utils::return_csv_headers();
 
-    let row_style = Style::default().fg(Color::Red);
-   
+
     let rows = csv_rows.iter().map(|values| {
-        Row::StyledData(values.iter(), row_style)
+        Row::Data(values.iter())
     });
 
     // Rendering data
-    let width = 20;
-    let constraints_vec = iter::repeat(width)
-        .take(columns.keys().len())
-        .map(|w| Constraint::Percentage(w))
-        .collect::<Vec<Constraint>>();
-
-    let chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .margin(1)
-        .constraints(
-            constraints_vec.as_ref(),
-        )
-        .split(f.size());
-
-    Block::default()
-        .borders(Borders::LEFT | Borders::BOTTOM)
-        .render(f, chunks[0]);
-    Block::default()
-        .borders(Borders::LEFT | Borders::BOTTOM)
-        .render(f, chunks[1]);
-    Block::default()
-        .borders(Borders::LEFT | Borders::BOTTOM)
-        .render(f, chunks[2]);
-    Block::default()
-        .borders(Borders::LEFT | Borders::BOTTOM)
-        .render(f, chunks[3]);
-    Block::default()
-        .borders(Borders::LEFT | Borders::RIGHT | Borders::BOTTOM)
-        .render(f, chunks[4]);
-
     let size = f.size();
-    Table::new(
+    // TODO: This should be made using our own custom built widget that extends this table with the
+    //       ability to draw borders and more
+    GriddedTable::new(
             headers.iter(),
             rows.into_iter()
         )
-        .header_style(Style::default().fg(Color::Yellow))
-        .widths(&[20, 20, 20, 20, 20])
+        .widths(&[
+            Constraint::Length(10),
+            Constraint::Length(10),
+            Constraint::Length(10),
+            Constraint::Length(10),
+            Constraint::Length(10),
+            Constraint::Length(10),
+            Constraint::Length(10),
+            Constraint::Length(10),
+            Constraint::Length(10),
+            Constraint::Length(10),
+        ])
         .style(Style::default().fg(Color::White))
         .column_spacing(2)
         .render(f, size);
@@ -87,7 +68,7 @@ fn main() {
     loop {
         terminal
             .draw(|mut f| {
-                draw_rows(&mut f);
+                draw_table(&mut f);
             })
             .unwrap();
 
---
+++ src/widgets/gridded_table.rs
@@ -0,0 +1,294 @@
+/// Holds data to be displayed in a Table widget
+pub enum Row<D, I>
+where
+    D: Iterator<Item = I>,
+    I: Display,
+{
+    Data(D),
+    StyledData(D, Style),
+}
+
+/// A widget to display data in formatted columns
+///
+/// # Examples
+///
+/// ```
+/// # use tui::widgets::{Block, Borders, Table, Row};
+/// # use tui::layout::Constraint;
+/// # use tui::style::{Style, Color};
+/// # fn main() {
+/// let row_style = Style::default().fg(Color::White);
+/// Table::new(
+///         ["Col1", "Col2", "Col3"].into_iter(),
+///         vec![
+///             Row::StyledData(["Row11", "Row12", "Row13"].into_iter(), row_style),
+///             Row::StyledData(["Row21", "Row22", "Row23"].into_iter(), row_style),
+///             Row::StyledData(["Row31", "Row32", "Row33"].into_iter(), row_style),
+///             Row::Data(["Row41", "Row42", "Row43"].into_iter())
+///         ].into_iter()
+///     )
+///     .block(Block::default().title("Table"))
+///     .header_style(Style::default().fg(Color::Yellow))
+///     .widths(&[Constraint::Length(5), Constraint::Length(5), Constraint::Length(10)])
+///     .style(Style::default().fg(Color::White))
+///     .column_spacing(1);
+/// # }
+/// ```
+pub struct GriddedTable<'a, T, H, I, D, R>
+where
+    T: Display,
+    H: Iterator<Item = T>,
+    I: Display,
+    D: Iterator<Item = I>,
+    R: Iterator<Item = Row<D, I>>,
+{
+    /// A block to wrap the widget in
+    block: Option<Block<'a>>,
+    /// Base style for the widget
+    style: Style,
+    /// Header row for all columns
+    header: H,
+    /// Style for the header
+    header_style: Style,
+    /// Width constraints for each column
+    widths: &'a [Constraint],
+    /// Space between each column
+    column_spacing: u16,
+    /// Data to display in each row
+    rows: R,
+}
+
+impl<'a, T, H, I, D, R> Default for GriddedTable<'a, T, H, I, D, R>
+where
+    T: Display,
+    H: Iterator<Item = T> + Default,
+    I: Display,
+    D: Iterator<Item = I>,
+    R: Iterator<Item = Row<D, I>> + Default,
+{
+    fn default() -> GriddedTable<'a, T, H, I, D, R> {
+        GriddedTable {
+            block: None,
+            style: Style::default(),
+            header: H::default(),
+            header_style: Style::default(),
+            widths: &[],
+            rows: R::default(),
+            column_spacing: 1,
+        }
+    }
+}
+
+impl<'a, T, H, I, D, R> GriddedTable<'a, T, H, I, D, R>
+where
+    T: Display,
+    H: Iterator<Item = T>,
+    I: Display,
+    D: Iterator<Item = I>,
+    R: Iterator<Item = Row<D, I>>,
+{
+    pub fn new(header: H, rows: R) -> GriddedTable<'a, T, H, I, D, R> {
+        GriddedTable {
+            block: None,
+            style: Style::default(),
+            header,
+            header_style: Style::default(),
+            widths: &[],
+            rows,
+            column_spacing: 1,
+        }
+    }
+    pub fn block(mut self, block: Block<'a>) -> GriddedTable<'a, T, H, I, D, R> {
+        self.block = Some(block);
+        self
+    }
+
+    pub fn header<II>(mut self, header: II) -> GriddedTable<'a, T, H, I, D, R>
+    where
+        II: IntoIterator<Item = T, IntoIter = H>,
+    {
+        self.header = header.into_iter();
+        self
+    }
+
+    pub fn header_style(mut self, style: Style) -> GriddedTable<'a, T, H, I, D, R> {
+        self.header_style = style;
+        self
+    }
+
+    pub fn widths(mut self, widths: &'a [Constraint]) -> GriddedTable<'a, T, H, I, D, R> {
+        let between_0_and_100 = |&w| match w {
+            Constraint::Percentage(p) => p <= 100,
+            _ => true,
+        };
+        assert!(
+            widths.iter().all(between_0_and_100),
+            "Percentages should be between 0 and 100 inclusively."
+        );
+        self.widths = widths;
+        self
+    }
+
+    pub fn rows<II>(mut self, rows: II) -> GriddedTable<'a, T, H, I, D, R>
+    where
+        II: IntoIterator<Item = Row<D, I>, IntoIter = R>,
+    {
+        self.rows = rows.into_iter();
+        self
+    }
+
+    pub fn style(mut self, style: Style) -> GriddedTable<'a, T, H, I, D, R> {
+        self.style = style;
+        self
+    }
+
+    pub fn column_spacing(mut self, spacing: u16) -> GriddedTable<'a, T, H, I, D, R> {
+        self.column_spacing = spacing;
+        self
+    }
+}
+
+impl<'a, T, H, I, D, R> Widget for GriddedTable<'a, T, H, I, D, R>
+where
+    T: Display,
+    H: Iterator<Item = T>,
+    I: Display,
+    D: Iterator<Item = I>,
+    R: Iterator<Item = Row<D, I>>,
+{
+    fn draw(&mut self, area: Rect, buf: &mut Buffer) {
+        // Render block if necessary and get the drawing area
+        let table_area = match self.block {
+            Some(ref mut b) => {
+                b.draw(area, buf);
+                b.inner(area)
+            }
+            None => area,
+        };
+
+        // Set the background
+        self.background(table_area, buf, self.style.bg);
+
+        let mut solver = Solver::new();
+        let mut var_indices = HashMap::new();
+        let mut ccs = Vec::new();
+        let mut variables = Vec::new();
+        for i in 0..self.widths.len() {
+            let var = cassowary::Variable::new();
+            variables.push(var);
+            var_indices.insert(var, i);
+        }
+        for (i, constraint) in self.widths.iter().enumerate() {
+            ccs.push(variables[i] | GE(WEAK) | 0.);
+            ccs.push(match *constraint {
+                Constraint::Length(v) => variables[i] | EQ(MEDIUM) | f64::from(v),
+                Constraint::Percentage(v) => {
+                    variables[i] | EQ(WEAK) | (f64::from(v * area.width) / 100.0)
+                }
+                Constraint::Ratio(n, d) => {
+                    variables[i] | EQ(WEAK) | (f64::from(area.width) * f64::from(n) / f64::from(d))
+                }
+                Constraint::Min(v) => variables[i] | GE(WEAK) | f64::from(v),
+                Constraint::Max(v) => variables[i] | LE(WEAK) | f64::from(v),
+            })
+        }
+        solver
+            .add_constraint(
+                variables
+                    .iter()
+                    .fold(Expression::from_constant(0.), |acc, v| acc + *v)
+                    | LE(REQUIRED)
+                    | f64::from(
+                        area.width - 2 - (self.column_spacing * (variables.len() as u16 - 1)),
+                    ),
+            )
+            .unwrap();
+        solver.add_constraints(&ccs).unwrap();
+        let mut solved_widths = vec![0; variables.len()];
+        for &(var, value) in solver.fetch_changes() {
+            let index = var_indices[&var];
+            let value = if value.is_sign_negative() {
+                0
+            } else {
+                value as u16
+            };
+            solved_widths[index] = value
+        }
+
+        let mut y = table_area.top();
+        let mut x = table_area.left();
+
+        // Draw header
+        if y < table_area.bottom() {
+            for (w, t) in solved_widths.iter().zip(self.header.by_ref()) {
+                buf.set_stringn(
+                    x + self.column_spacing,
+                    y + 1,
+                    format!("{}", t),
+                    *w as usize,
+                    self.header_style,
+                );
+                x += *w + self.column_spacing + 2;
+            }
+        }
+        y += 4;
+
+        // Draw rows
+        let default_style = Style::default();
+        if y < table_area.bottom() {
+            let remaining = (table_area.bottom() - y) as usize;
+
+            for (i, row) in self.rows.by_ref().take(remaining).enumerate() {
+                let (data, style) = match row {
+                    Row::Data(d) => (d, default_style),
+                    Row::StyledData(d, s) => (d, s),
+                };
+                x = table_area.left();
+
+                for x in table_area.left()..table_area.right() {
+                    buf.get_mut(x, table_area.top())
+                        .set_symbol(line::HORIZONTAL);
+                }
+                for x in table_area.left()..table_area.right() {
+                    buf.get_mut(x, table_area.top() + y - 2)
+                        .set_symbol(line::HORIZONTAL);
+                }
+                for y in table_area.top()..table_area.bottom() {
+                    buf.get_mut(table_area.right() - 1, y)
+                        .set_symbol(line::VERTICAL);
+                }
+                for x in table_area.left()..table_area.right() {
+                    buf.get_mut(x, table_area.bottom() - 1)
+                        .set_symbol(line::HORIZONTAL);
+                }
+
+                for (w, elt) in solved_widths.iter().zip(data) {
+                    // Draw Border sides
+                    // Draw border Corners
+                    for y in table_area.top()..table_area.bottom() {
+                        buf.get_mut(table_area.left() + x, y)
+                            .set_symbol(line::VERTICAL);
+                    }
+
+                    buf.set_stringn(
+                        x + 2,
+                        y - 1 + i as u16,
+                        format!("{}", elt),
+                        *w as usize,
+                        style,
+                    );
+                    x += *w + self.column_spacing + 2;
+                }
+            }
+            // Draw Table corners
+            buf.get_mut(table_area.left(), table_area.top())
+                .set_symbol(line::TOP_LEFT);
+            buf.get_mut(table_area.right() - 1, table_area.top())
+                .set_symbol(line::TOP_RIGHT);
+            buf.get_mut(table_area.left(), table_area.bottom() - 1)
+                .set_symbol(line::BOTTOM_LEFT);
+            buf.get_mut(table_area.right() - 1, table_area.bottom() - 1)
+                .set_symbol(line::BOTTOM_RIGHT);
+        }
+    }
+}
